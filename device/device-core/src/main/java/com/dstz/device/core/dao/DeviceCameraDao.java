725b8ca96e9657b17bf35d0d477af9fba223bdc7
30895 <mengenxu@yeah.net>
2019-10-10 09:03:14 +0800

    测试

--- device/device-core/src/main/java/com/dstz/device/core/dao/DeviceCameraDao.java
+++ device/device-core/src/main/java/com/dstz/device/core/dao/DeviceCameraDao.java
@@ -7,7 +7,16 @@ import com.dstz.device.core.model.DeviceCamera;
 
 public interface DeviceCameraDao extends BaseDao<String, DeviceCamera> {
 
+    /**
+     * 根据基础设备ID获取视频监控
+     * @param basicId
+     * @return
+     */
     DeviceCamera getByDeviceBaiscId(String basicId);
 
+    /**
+     * 根据基础设备ID删除视频监控信息
+     * @param basicId
+     */
     void  removeByBasicId(String basicId);
 }
