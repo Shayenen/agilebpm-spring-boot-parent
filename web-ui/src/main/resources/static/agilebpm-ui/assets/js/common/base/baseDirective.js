99c4dc2aa095d24a53dfcf3f3985e6eb85fa1422
SheldonFei <lufei1993@126.com>
2019-10-11 15:38:09 +0800

    前端增加日期组件到设备新增

--- web-ui/src/main/resources/static/agilebpm-ui/assets/js/common/base/baseDirective.js
+++ web-ui/src/main/resources/static/agilebpm-ui/assets/js/common/base/baseDirective.js
@@ -537,6 +537,7 @@ var directive = angular.module("baseDirective", [ "base" ])
 					  //console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
 					  //console.log(endDate); //得结束的日期时间对象，开启范围选择（range: true）才会返回。对象成员同上。
 				  }
+				  ,trigger: 'click'
 			});
 			
 			$ctrl.$formatters.push(function(value) {
